95912c8d1dbd03f5e181f158368a7b518181ac81
Jason Senich <jsenich@gmail.com>
2020-12-02 23:33:42 -0500

    Remove unused code

--- day02.py
+++ day02.py
@@ -16,7 +16,6 @@ def check_position_policy(password: str, policy_character: str, args: List[str])
     return character_counts.get(policy_character, 0) == 1
 
 def get_parsed_policy_line(password_line: str) -> Tuple[str, str, List[str]]:
-    parsed_policy = {}
     policy_params, password = password_line.split(': ')
     policy_args, letter = policy_params.split()
 
@@ -41,7 +40,6 @@ def part_two(puzzle_input: List[str]) -> int:
     return len(valid_passwords)
 
 if __name__ == '__main__':
-    passwords = []
     with open('day02_input.txt', 'r') as f:
         puzzle_input = [line for line in f.readlines() if line]
 
