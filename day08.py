d58fc2f2da5277a49feee5f6456cfa3c421c6647
Jay Senich <1821282+jsenich@users.noreply.github.com>
2020-12-09 23:31:24 -0500

    First pass at refactoring day 8 code

--- day08.py
+++ day08.py
@@ -1,22 +1,67 @@
+from typing import List, Set, Tuple
+
+
+class Program:
+    def __init__(self, instructions: List[Tuple[str, int]]) -> None:
+        self.instructions = instructions
+        self.executed_pointers: Set[int] = set()
+        self.change_attempted: Set[int] = set()
+
+        self.accumulator: int
+        self.instruction_pointer: int
+        self.memory: List[Tuple[str, int]]
+
+        self.initialize()
+
+    def initialize(self) -> None:
+        self.accumulator = 0
+        self.executed_pointers.clear()
+        self.instruction_pointer = 0
+        self.memory = self.instructions.copy()
+
+    def run(self, correct_errors: bool = False) -> Tuple[int, int]:
+        while self.instruction_pointer < len(self.memory):
+            if self.instruction_pointer in self.executed_pointers:
+                if correct_errors:
+                    self.initialize()
+                else:
+                    return 1, self.accumulator
+
+            self.executed_pointers.add(self.instruction_pointer)
+
+            op, argument = self.memory[self.instruction_pointer]
+
+            if op == 'acc':
+                self._execute_acc(argument)
+            elif op == 'jmp':
+                self._execute_jmp(argument)
+            else:  # nop
+                self._execute_nop()
+
+        return 0, self.accumulator
+
+    def _execute_acc(self, argument: int) -> None:
+        self.accumulator += argument
+        self.instruction_pointer += 1
+
+    def _execute_jmp(self, argument: int) -> None:
+        self.accumulator += argument
+
+    def _execute_nop(self) -> None:
+        self.instruction_pointer += 1
+
+
+def get_instructions(puzzle_input: str) -> List[Tuple[str, int]]:
+    instructions = []
+    for line in puzzle_input.strip().split('\n'):
+        op, arg = line.split(' ')
+        instructions.append((op, int(arg)))
+    return instructions
+
 
 def part_one(puzzle_input) -> int:
-    accumulator = 0
-    instruction_pointer = 0
-    instructions = puzzle_input.strip().split('\n')
-    visited = set()
-    while instruction_pointer not in visited:
-        visited.add(instruction_pointer)
-        op, argument = instructions[instruction_pointer].split(' ')
-
-        if op == 'nop':
-            instruction_pointer += 1
-            continue
-
-        if op == 'acc':
-            accumulator += int(argument)
-            instruction_pointer += 1
-        elif op == 'jmp':
-            instruction_pointer += int(argument)
+    program = Program(get_instructions(puzzle_input))
+    _, accumulator = program.run(correct_errors=False)
 
     return accumulator
 
