e1a94d835865684a9e5341ba7e17ef7a9f198862
Jay Senich <1821282+jsenich@users.noreply.github.com>
2020-12-10 21:14:42 -0500

    Completed day 10

--- day10.py
+++ day10.py
@@ -1,4 +1,5 @@
-from collections import Counter
+from collections import Counter, defaultdict
+from typing import Dict, List
 
 test_input = """16
 10
@@ -14,34 +15,60 @@ test_input = """16
 """
 
 
-
-if __name__ == '__main__':
-    with open('day10_input.txt', 'r') as f:
-        puzzle_input = [int(i) for i in f.read().strip().split()]
-
-
-    # adapters = sorted([int(n) for n in test_input.strip().split('\n')])
-    adapters = sorted(puzzle_input)
+def part_one(puzzle_input: List[int]) -> Dict[str, int]:
+    joltages = sorted(puzzle_input)
     adapter_pointer = 0
-    adapter_rating = max(adapters) + 3
+    adapter_rating = max(joltages) + 3
     previous_output = 0
     used_adapters = set()
     differences = Counter()
 
-    adapters.append(adapter_rating)
+    joltages.append(adapter_rating)
 
-    current_adapter = adapters[adapter_pointer]
+    current_adapter = joltages[adapter_pointer]
     while current_adapter not in used_adapters:
-        if adapter_pointer > len(adapters):
+        if adapter_pointer > len(joltages):
             adapter_pointer = 0
             continue
 
-        if adapters[adapter_pointer] - 3 <= previous_output:
-            used_adapters.add(adapters[adapter_pointer])
-            differences.update(str(adapters[adapter_pointer] - previous_output))
-            previous_output = adapters[adapter_pointer]
-            if adapter_pointer + 1 < len(adapters):
-                current_adapter = adapters[adapter_pointer + 1]
+        if joltages[adapter_pointer] - 3 <= previous_output:
+            used_adapters.add(joltages[adapter_pointer])
+            differences.update(str(joltages[adapter_pointer] - previous_output))
+            previous_output = joltages[adapter_pointer]
+            if adapter_pointer + 1 < len(joltages):
+                current_adapter = joltages[adapter_pointer + 1]
         adapter_pointer += 1
 
+    return dict(differences)
+
+
+def part_two(puzzle_input: List[int]) -> int:
+    joltages = sorted([0] + puzzle_input + [max(puzzle_input) + 3])
+
+    variations_map = defaultdict(list)
+    for i, joltage in enumerate(joltages):
+        variations = []
+        for jolt_diff in range(1, 4):
+            variations.append(joltage + jolt_diff)
+        for joltage2 in joltages:
+            if joltage2 in variations:
+                variations_map[joltage].append(joltage2)
+
+    adapter_combinations = defaultdict(int)
+    adapter_combinations[0] = 1
+    for joltage, variations in variations_map.items():
+        for variation in variations:
+            adapter_combinations[variation] += adapter_combinations[joltage]
+
+    return adapter_combinations[max(joltages)]
+
+
+if __name__ == '__main__':
+    with open('day10_input.txt', 'r') as f:
+        puzzle_input = [int(i) for i in f.read().strip().split()]
+        test_input = [int(i) for i in test_input.strip().split()]
+
+    differences = part_one(puzzle_input)
+
     print(f'Part 1: {differences["1"] * differences["3"]}')
+    print(part_two(puzzle_input))
