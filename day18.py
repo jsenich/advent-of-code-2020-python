21594367a45f060c5f7cc7edc9f8d51e4fc96c3e
Jay Senich <1821282+jsenich@users.noreply.github.com>
2020-12-18 22:30:46 -0500

    Completed day 18 part 2

--- day18.py
+++ day18.py
@@ -1,4 +1,8 @@
-import operator, re
+import operator
+import re
+from functools import reduce
+from operator import add, mul
+
 
 test_input = """1 + 2 * 3 + 4 * 5 + 6
 """
@@ -6,9 +10,23 @@ test_input = """1 + 2 * 3 + 4 * 5 + 6
 test_input = """((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
 """
 
+test_input = """(2 + 7 * (3 + 5 * 2 + 3 + 6) * (4 * 6 + 8 * 3 * 2 + 7)) + 3 * ((2 + 8 + 6) * 8 + 9 * 6 * 6) + 5 + 6 + 5
+"""
+
+#  ((6 * 9) * (15 * 14) + 6) + 6 * 2
+#  (54 * 210 + 6) + 6 * 2
+#  (54 * 216) + 6 * 2
+#  11664 + 6 * 2
+#  11670 * 2
+#  23340
+
+
+# test_input = """1 + (2 * 3) + (4 * (5 + 6))
+# """
+
 op_map = {
-    '+': operator.add,
-    '*': operator.mul,
+    '+': add,
+    '*': mul,
 }
 
 
@@ -28,6 +46,7 @@ def evaluate_expression_string(expression_string: str):
 
     return result
 
+
 def part_one(puzzle_input):
     result = 0
     for expression in puzzle_input.strip().split('\n'):
@@ -39,9 +58,69 @@ def part_one(puzzle_input):
     return result
 
 
+def process_additions(expression_string: str):
+    expression = expression_string
+    while additions := re.findall(r'\d+\s\+\s\d+(?:\s\+\s\d+)*', expression):
+        for addition in additions:
+            nums = [int(num) for num in addition.split(' + ')]
+            expression = expression.replace(addition, str(sum(nums)).replace('(', '').replace(')', ''))
+    return expression
+
+
+def process_products(expression_string: str):
+    expression = expression_string
+    while products := re.findall(r'\d+\s\*\s\d+(?:\s\*\s\d+)*', expression):
+        for product in products:
+            nums = [int(num) for num in product.split(' * ')]
+            expression = expression.replace(product, str(reduce(operator.__mul__, nums)).replace('(', '').replace(')', ''))
+    return expression
+
+
+def strip_noop_expressions(expression_string: str):
+    expression = expression_string
+    while noops := re.findall(r'\(\d+\)', expression):
+        for noop in noops:
+            expression = expression.replace(noop, noop.replace('(', '').replace(')', ''))
+
+    return expression
+
+# (2 + 7 * (3 + 5 * 2 + 3 + 6) * (4 * 6 + 8 * 3 * 2 + 7)) + 3 * ((2 + 8 + 6) * 8 + 9 * 6 * 6) + 5 + 6 + 5
+# (9 * (8 * 11) * (4 * 14 * 3 * 9)) + 3 * (16 * 17 * 6 * 6) + 16
+# (9 * 88 * 1512) + 3 * 9792 + 16
+# 1197504 + 3 * 9808
+# 1197507 * 9808
+# 11745148656
+
+
+
+def part_two(puzzle_input: str):
+    result = 0
+    for expression in puzzle_input.strip().split('\n'):
+        processing = True
+        while processing:
+            # if expression.count('(') <= 1:
+            #     expression = re.sub(r'[\(\)]', '', expression)
+            #     processing = False
+            #     continue
+
+            if subexpressions := re.findall(r'\([\d\s\+\*]+\)', expression):
+                for subexpression in subexpressions:
+                    new_expression = process_products(process_additions(subexpression))
+                    if re.match(r'^\(\d+\)$', new_expression):
+                        new_expression = re.sub(r'[\(\)]', '', new_expression)
+                    expression = expression.replace(subexpression, new_expression)
+            else:
+                processing = False
+            # print(expression)
+        result += int(process_products(process_additions(expression)))
+
+    return result
+
+
 if __name__ == '__main__':
     with open('day18_input.txt', 'r') as f:
         puzzle_input = f.read()
     # puzzle_input = test_input.strip()
 
     print(f'Part 1: {part_one(puzzle_input)}')
+    print(f'Part 2: {part_two(puzzle_input)}')
